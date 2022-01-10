cfc5fd88d57d5cf45a4f643130d42299339158a2
Fernando <fst_96@hotmail.com>
2022-01-10 16:50:21 -0600

    Anadiendo cuadricula

--- gato-react/src/components/App.js
+++ gato-react/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { GridGato } from './GridGato'
 
 export const App = () => {
     return (
         <div>
             <h1>Hola amigos, desde el juego de tic tac toc</h1>
+            <GridGato/>
         </div>
     )
 }
---
+++ gato-react/src/components/GridGato.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const GridGato = () => {
+
+
+
+
+
+    
+    return (
+        <div className="container">
+            <div className="cuadricula">
+                <div className="cuadricula__celdas cuadricula__celda--1"></div>
+                <div className="cuadricula__celdas cuadricula__celda--2"></div>
+                <div className="cuadricula__celdas cuadricula__celda--3"></div>
+                <div className="cuadricula__celdas cuadricula__celda--4"></div>
+                <div className="cuadricula__celdas cuadricula__celda--5"></div>
+                <div className="cuadricula__celdas cuadricula__celda--6"></div>
+                <div className="cuadricula__celdas cuadricula__celda--7"></div>
+                <div className="cuadricula__celdas cuadricula__celda--8"></div>
+                <div className="cuadricula__celdas cuadricula__celda--9"></div>
+            </div>
+            {/* TODO Colocar otro componente para el chat */}
+        </div>
+    )
+}
